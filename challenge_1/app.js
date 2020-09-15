ab747e54ef2198b6a0fc47946454cf6591111a72
Daniel Flores <danielflores125@gmail.com>
2020-09-14 17:10:32 -0700

    Completed checkColumns

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,40 +1,63 @@
 
 count = 0
-function myFunction(id){
-  if(count % 2 === 0){
-  document.getElementById(id).textContent = 'X';
-  count++
+var board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-  } else {
-    document.getElementById(id).textContent = 'O';
-    count++
+
+
+function myFunction(id) {
+  var toNumber = Number(id)
+  if (document.getElementById(id).innerText.length === 0) {
+    if (count % 2 === 0) {
+      document.getElementById(id).textContent = 'X';
+      count++
+      board[toNumber - 1] = 1
+    } else {
+      document.getElementById(id).textContent = 'O';
+      count++
+      board[toNumber - 1] = 2
+    }
+    console.log(board)
   }
-  if(count === 9){
-    document.getElementById("console").textContent = 'Winner'
+  gameplay(id)
+}
+
+
+
+
+function gameplay(checkingId) {
+  checkColumns(checkingId)
+}
+
+
+function checkColumns(checkingId) {
+  var checking = Number(checkingId) - 1
+  console.log(checking)
+
+  if (board[checking] === board[checking + 3] && board[checking + 6]) {
+    console.log('winner')
+  } else if (board[checking] === board[checking - 3] && board[checking - 6]) {
+    console.log('you won')
   }
 }
 
-function clearBoard (){
-  //var x = document.getElementById("1").innerText;
+function checkRows(checkingId) {
+  var checking = Number(checkingId) - 1
+  console.log(checking)
 
+  if (board[checking] === board[checking + 1] && board[checking + 2]) {
+    console.log('winner')
+  } else if (board[checking] === board[checking - 1] && board[checking - 2]) {
+    console.log('you won')
+  }
 }
 
 
-function gameplay(){
-console.log('its working')
-var position1 = document.getElementById(1).innerText
-var position2 = document.getElementById(2).innerText
-var position3 = document.getElementById(3).innerText
-console.log(position1)
-console.log(position2)
-console.log(position3)
-if(position1 && position2 && position3 === 'X'){
-  console.log('winner')
-  // document.getElementById("console").textContent = 'X Wins'
-}
-}
 
 
+
+function clearBoard() {
+
+}
 
 
 
