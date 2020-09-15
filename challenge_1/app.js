acd4a0ac654af56668d369f8d38cfa4ce191e58a
Daniel Flores <danielflores125@gmail.com>
2020-09-14 17:26:09 -0700

    Completed checkRows and checkColumns

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -26,6 +26,7 @@ function myFunction(id) {
 
 function gameplay(checkingId) {
   checkColumns(checkingId)
+  checkRows(checkingId)
 }
 
 
@@ -42,18 +43,20 @@ function checkColumns(checkingId) {
 
 function checkRows(checkingId) {
   var checking = Number(checkingId) - 1
-  console.log(checking)
-
-  if (board[checking] === board[checking + 1] && board[checking + 2]) {
+  if(board[checking] === board[checking - 1] && board[checking + 1]){
     console.log('winner')
-  } else if (board[checking] === board[checking - 1] && board[checking - 2]) {
-    console.log('you won')
+  }
+  if(board[checking] === board[checking + 1] && board[checking + 2]){
+    console.log('win again')
+  }
+  if(board[checking] === board[checking - 1] && board[checking - 2]){
+    console.log('you win again')
   }
 }
 
+function diagonal(){
 
-
-
+}
 
 function clearBoard() {
 
